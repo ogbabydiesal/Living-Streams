2b7fb5b0f7f9c16805a6539ccca635a2b6442d51
tommy martinez <thomasjohnmartinez@gmail.com>
2023-07-26 19:34:45 -0400

    polyphony
    works great have to refactor so that I create the players in a loop and also create polyphony for the higher voices

--- js/cluster.js
+++ js/cluster.js
@@ -4,16 +4,30 @@ let ranSample = 0;
 let counter = 0;
 
 const vol = new Tone.Volume(-12).toDestination();
-const reverb = new Tone.Reverb({decay: 9, wet: .7}).connect(vol);
+const reverb = new Tone.Reverb({decay: 4, wet: .6}).connect(vol);
 const fDel = new Tone.FeedbackDelay(".143", 0.6).connect(reverb);
-const fDel2 = new Tone.FeedbackDelay(".6", 0.5).connect(reverb);
-
+const fDel2 = new Tone.FeedbackDelay(".25", 0.5).connect(reverb);
+const silentPlayer = new Tone.Player("./sounds/silence.m4a");
+silentPlayer.connect(reverb);
+silentPlayer.loop = true;
 mainPlayer = new Tone.Player().connect(reverb);
+mainPlayer2 = new Tone.Player().connect(reverb);
+mainPlayer3 = new Tone.Player().connect(reverb);
+mainPlayer4 = new Tone.Player().connect(reverb);
+mainPlayers = [mainPlayer, mainPlayer2 ,mainPlayer3, mainPlayer4];
 player = new Tone.Player().connect(fDel);
 player2 = new Tone.Player().connect(fDel);
+player3 = new Tone.Player().connect(fDel);
 player4 = new Tone.Player().connect(fDel2);
+player5 = new Tone.Player().connect(fDel2);
+player6 = new Tone.Player().connect(fDel2);
+players = [player, player2, player3, player4, player5, player6];
+playerCount = 0;
+mainPlayerCount = 0;
+function getRandomInt(max) {
+  return Math.floor(Math.random() * max) + 1;
+}
 
-const silentPlayer = new Tone.Player("./sounds/silence.m4a");
 const samples = new Tone.ToneAudioBuffers({
   0 : "sounds/root/sample_0.mp3",
   1 : "sounds/gtr/cluster_1/sample_1.mp3",
@@ -66,85 +80,80 @@ const samples = new Tone.ToneAudioBuffers({
 	
 }, () => {
   document.querySelector(".button").innerHTML = "play";
-  
-  silentPlayer.connect(reverb);
-  silentPlayer.loop = true;
-  silentPlayer.start();
-
 });
 
-function getRandomInt(max) {
-  return Math.floor(Math.random() * max) + 1;
-}
-
-let x = 0;
+let x = 0; //keep track of playhead drawing
 let isDrawing = false;
-function increment(evt) {
-  let canvas = document.querySelector("canvas");
-  let ctx = canvas.getContext("2d");
-  canvas.addEventListener('mousedown', e => {
+let canvas = document.querySelector("canvas");
+let ctx = canvas.getContext("2d");
+canvas.addEventListener('mousedown', e => {
+  x = e.offsetX;
+  counter = x;
+  isDrawing = true;
+});
+canvas.addEventListener('mousemove', e => {
+  if (isDrawing === true) {
     x = e.offsetX;
     counter = x;
-    isDrawing = true;
+  }
+  canvas.addEventListener('mouseup', e => {
+    isDrawing = false;
   });
-  canvas.addEventListener('mousemove', e => {
-    if (isDrawing === true) {
-      x = e.offsetX;
-      counter = x;
-    }
-    canvas.addEventListener('mouseup', e => {
-      isDrawing = false;
-    });
-  });
-  ctx.fillStyle = "black";
+});
+ctx.fillStyle = "black";
+function increment(evt) {
   if(isPlaying) {
     counter = counter + .14;
-    
-    if (window.innerWidth > 600) {
-      canvas.width = 290;
-      canvas.height = 28;
-    }
-    else {
-      canvas.width = 100;
-    }
     ctx.clearRect(0, 0, canvas.width, canvas.height)
     ctx.fillRect(counter, 0, 1, canvas.height)
     //the low d
     if (counter > 6) { 
-    if (Math.floor(counter) % 4 == 0 && Math.random() > Math.random()*.8) {
-        mainPlayer.start();    
-    }
-    if (Math.floor(counter) % 5 == 0 && Math.random() > Math.random()*.8) {
-      try{
-        player2.buffer = samples.get(getRandomInt(25));
-        player2.start();
-      }
-      catch(error){}  
-        
-    }
-    if (Math.floor(counter) % 7 == 0 && Math.random() > Math.random()*.2) {
-      try{
-        player.buffer = samples.get(getRandomInt(25));
-        player.start();
-      }
-      catch(error){}
-    }
-    
-    if (counter > 100 && counter < 280) {
-      if (Math.floor(counter) % 2 == 0 && Math.random() > Math.random()*.2) {
-        try{
-          player4.buffer = samples.get(getRandomInt(20) + 26);
-          player4.start();
+      if (Math.floor(counter) % 4 == 0 && Math.random() > Math.random()*.8) {
+        mainPlayerCount++;
+        if (mainPlayerCount > 3) {
+          mainPlayerCount = 0;
         }
-        catch(error){}
+        
+        mainPlayers[mainPlayerCount].buffer = samples.get("0");
+        mainPlayers[mainPlayerCount].start();    
       }
-    }
-    if (counter >= 290) {
-      Tone.Transport.stop();
-      document.querySelector(".button").innerHTML = "play";
-      isPlaying = false;
-    }
-    
+      if (Math.floor(counter) % 5 == 0 && Math.random() > Math.random()*.8) {
+        playerCount++;
+        console.log(playerCount);
+        try{
+          
+          if (playerCount > 6) {
+            playerCount = 0;
+          }
+          players[playerCount].buffer = samples.get(getRandomInt(25));
+          players[playerCount].start();
+        } catch(error){}    
+      }
+      if (Math.floor(counter) % 7 == 0 && Math.random() > Math.random()*.2) {
+        playerCount++;
+        console.log(playerCount);
+        try{
+          
+          if (playerCount > 6) {
+            playerCount = 0;
+          }
+          players[playerCount].buffer = samples.get(getRandomInt(25));
+          players[playerCount].start();
+        } catch(error){} 
+      }
+      if (counter > 100 && counter < 280) {
+        if (Math.floor(counter) % 2 == 0 && Math.random() > Math.random()*.2) {
+          try{
+            player4.buffer = samples.get(getRandomInt(20) + 26);
+            player4.start();
+          } catch(error){}
+        }
+      }
+      if (counter >= 290) {
+        Tone.Transport.stop();
+        document.querySelector(".button").innerHTML = "play";
+        isPlaying = false;
+      }
   }
 }
   setTimeout(increment, (Math.random() * 60 + 70));
@@ -159,10 +168,9 @@ function init() {
     document.querySelector(".button").innerHTML = "pause";
     Tone.start();
     Tone.Transport.start();
-    const timer = setTimeout(increment, 100);
+    silentPlayer.start();
     mainPlayer.buffer = samples.get("0");
-    player.buffer = samples.get("2");
-    player2.buffer = samples.get("5");
+    increment();
   }
   else {
     Tone.Transport.pause();
@@ -170,3 +178,14 @@ function init() {
     isPlaying = false;
   }
 }
+function adjustCanvas() {
+  if (window.innerWidth > 600) {
+    canvas.width = 290;
+    canvas.height = 28;
+  }
+  else {
+    canvas.width = 100;
+  }
+}
+
+window.onresize = adjustCanvas;
